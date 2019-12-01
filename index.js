e1816facaeb7a83fba3236ba046bb0d99a84af1b
ban hussein <banhussien97@gmail.com>
2019-12-01 16:46:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+Function writecards(["Ada", "Brendan", "Ali"], "birthday")
+{
+
+  for(let i=0;i<arr.length;i++){
+     
+     let  msg[i]=(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+}
+return msg;
+}
