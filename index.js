c2019c2cadcf671e472f0c78929b8c123312a0ad
shams khalid <shamsnaji4@gmail.com>
2019-12-13 11:49:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+function writeCards(arr [3] , str){
+  
+  for(let i=0;i<arr.length;i++)
+  return console.log(`Thanks ${arr[i]}`);
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+for (let countdown = 10; countdown < 0; countdown--) {
+  console.log(countdown);
+}
